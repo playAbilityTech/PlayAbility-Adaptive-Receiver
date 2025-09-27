54d28dc801f7a31e03c9be2437db2460c347baf6
Valentin Squirelo <valentin+github@hackerloop.com>
2025-09-27 19:01:49 +0200

    remove unused hardware includes and get_bootsel_button function

--- receiver-pico/src/receiver.c
+++ receiver-pico/src/receiver.c
@@ -9,9 +9,6 @@
 #include "hardware/gpio.h"
 #include "hardware/uart.h"
 #include "hardware/watchdog.h"
-#include "hardware/sync.h"
-#include "hardware/structs/ioqspi.h"
-#include "hardware/structs/sio.h"
 
 #include "pico/stdio.h"
 
@@ -113,26 +110,6 @@ outgoing_report_t outgoing_reports[OR_BUFSIZE];
 uint8_t or_head = 0;
 uint8_t or_tail = 0;
 uint8_t or_items = 0;
-
-bool __no_inline_not_in_flash_func(get_bootsel_button)() {
-    const uint CS_PIN_INDEX = 1;
-    uint32_t flags = save_and_disable_interrupts();
-    hw_write_masked(&ioqspi_hw->io[CS_PIN_INDEX].ctrl,
-                    GPIO_OVERRIDE_LOW << IO_QSPI_GPIO_QSPI_SS_CTRL_OEOVER_LSB,
-                    IO_QSPI_GPIO_QSPI_SS_CTRL_OEOVER_BITS);
-    for (volatile int i = 0; i < 1000; ++i);
-#if PICO_RP2040
-    #define CS_BIT (1u << 1)
-#else
-    #define CS_BIT SIO_GPIO_HI_IN_QSPI_CSN_BITS
-#endif
-    bool button_state = !(sio_hw->gpio_hi_in & CS_BIT);
-    hw_write_masked(&ioqspi_hw->io[CS_PIN_INDEX].ctrl,
-                    GPIO_OVERRIDE_NORMAL << IO_QSPI_GPIO_QSPI_SS_CTRL_OEOVER_LSB,
-                    IO_QSPI_GPIO_QSPI_SS_CTRL_OEOVER_BITS);
-    restore_interrupts(flags);
-    return button_state;
-}
 
 void queue_outgoing_report(uint8_t report_id, uint8_t* data, uint8_t len) {
     if (or_items == OR_BUFSIZE) {
